18d4a74b65dab473926b2c09a89d59a08fdbd6e2
Jason <jasonetco@gmail.com>
2016-10-24 18:17:55 -0400

    Use chrome tabs query to find URL

--- extension/js/popup.js
+++ extension/js/popup.js
@@ -2,7 +2,31 @@ document.querySelector('#go-to-options').onclick = () => {
   chrome.runtime.openOptionsPage();
 };
 
+function extractDomain(url) {
+    let domain;
+    let protocol;
+    // Find & remove protocol (http, ftp, etc.) and get domain
+    if (url.indexOf("://") > -1) {
+      domain = url.split('/')[2];
+      protocol = url.split('/')[0];
+
+      console.log(domain, protocol);
+    } else {
+      domain = url.split('/')[0];
+      console.log(domain, "NOPE!");
+    }
+    // Find & remove port number
+    domain = domain.split(':')[0];
+    return protocol + domain;
+}
+
 document.querySelector('#disable').onclick = () => {
+  chrome.tabs.query({
+    active: true,
+    currentWindow: true
+  }, tab => {
+    console.log(tab[0].url);
+  });
   const url = `${location.protocol}//${location.host}`;
   let arr;
 
